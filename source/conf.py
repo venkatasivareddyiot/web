cc82aed0eb674f39390f444d155ed44a08ad25b1
venkatasivareddyiot <47242203+venkatasivareddyiot@users.noreply.github.com>
2020-05-04 11:47:41 +0530

    Update conf.py
    updated

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ language = '.rst'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['recommonmark']
 
 
 # -- Options for HTML output -------------------------------------------------
